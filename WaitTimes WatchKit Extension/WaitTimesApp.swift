6884e8262ba185012c9cd629f7e5280d36a8ed54
Bruce Evans <bruce.evans.dev@gmail.com>
2021-02-20 12:05:31 +0900

    Initial Commit

---
+++ WaitTimes WatchKit Extension/WaitTimesApp.swift
@@ -0,0 +1,19 @@
+//
+//  WaitTimesApp.swift
+//  WaitTimes WatchKit Extension
+//
+//  Created by Bruce Evans on 2021/02/20.
+//
+
+import SwiftUI
+
+@main
+struct WaitTimesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
