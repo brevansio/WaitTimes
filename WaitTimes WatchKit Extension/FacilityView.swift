1c657f1a0a6a92f17ec2e7f1de53bdb860931a7d
Bruce Evans <bruce.evans.dev@gmail.com>
2021-02-21 13:57:40 +0900

    Change some colors

--- WaitTimes WatchKit Extension/FacilityView.swift
+++ WaitTimes WatchKit Extension/FacilityView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 private extension Color {
     static let mickeyRed = Color(.displayP3, red: 242 / 255, green: 5 / 255, blue: 5 / 255, opacity: 1)
     static let mickeyYellow = Color(.displayP3, red: 219 / 255, green: 215 / 255, blue: 11 / 255, opacity: 1)
-    static let mickeyBlack = Color(.displayP3, red: 0, green: 0, blue: 0, opacity: 1)
+    static let mickeyBlack = Color(.displayP3, red: 25 / 255, green: 25 / 255, blue: 25 / 255, opacity: 1)
 }
 
 struct FacilityView: View {
@@ -23,7 +23,7 @@ struct FacilityView: View {
                 .cornerRadius(15.0)
             VStack {
                 Text(facility.name)
-                    .foregroundColor(.mickeyYellow)
+                    .foregroundColor(.white)
                     .font(.subheadline)
                     .lineLimit(2)
                 HStack {
@@ -49,6 +49,6 @@ struct FacilityView: View {
             }
         }
         .background(Color.mickeyBlack)
-        .cornerRadius(3.0)
+        .cornerRadius(15.0)
     }
 }
