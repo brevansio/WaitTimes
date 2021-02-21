7ffde1e63e962bdc77c545aee34c1d377b5d83af
Bruce Evans <bruce.evans.dev@gmail.com>
2021-02-21 19:37:28 +0900

    Get simple complications working

--- WaitTimes WatchKit Extension/ComplicationController.swift
+++ WaitTimes WatchKit Extension/ComplicationController.swift
@@ -6,7 +6,11 @@
 //
 
 import ClockKit
+import SwiftUI
 
+extension UIColor {
+    static let tdlTheme = UIColor(displayP3Red: 197 / 255, green: 135 / 255, blue: 164 / 255, alpha: 1)
+}
 
 class ComplicationController: NSObject, CLKComplicationDataSource {
     
@@ -14,7 +18,10 @@ class ComplicationController: NSObject, CLKComplicationDataSource {
 
     func getComplicationDescriptors(handler: @escaping ([CLKComplicationDescriptor]) -> Void) {
         let descriptors = [
-            CLKComplicationDescriptor(identifier: "complication", displayName: "WaitTimes", supportedFamilies: CLKComplicationFamily.allCases)
+            CLKComplicationDescriptor(identifier: "complication",
+                                      displayName: "WaitTimes",
+                                      supportedFamilies: [.circularSmall, .graphicCircular, .graphicCorner,
+                                                          .modularSmall, .utilitarianSmall, .utilitarianSmallFlat])
             // Multiple complication support can be added here with more descriptors
         ]
         
@@ -42,18 +49,81 @@ class ComplicationController: NSObject, CLKComplicationDataSource {
     
     func getCurrentTimelineEntry(for complication: CLKComplication, withHandler handler: @escaping (CLKComplicationTimelineEntry?) -> Void) {
         // Call the handler with the current timeline entry
-        handler(nil)
-    }
-    
-    func getTimelineEntries(for complication: CLKComplication, after date: Date, limit: Int, withHandler handler: @escaping ([CLKComplicationTimelineEntry]?) -> Void) {
-        // Call the handler with the timeline entries after the given date
-        handler(nil)
+
+        let template: CLKComplicationTemplate
+
+        switch complication.family {
+        case .circularSmall:
+            let imageProvider = CLKImageProvider(onePieceImage: UIImage(named: "Complication/Circular")!)
+            template = CLKComplicationTemplateCircularSmallSimpleImage(imageProvider: imageProvider)
+        case .graphicCircular:
+            template = CLKComplicationTemplateGraphicCircularView(Image("Complication/Circular"))
+        case .graphicCorner:
+            let imageProvider = CLKFullColorImageProvider(fullColorImage: UIImage(named: "Complication/Graphic Corner")!)
+            template = CLKComplicationTemplateGraphicCornerCircularImage(imageProvider: imageProvider)
+        case .modularSmall:
+            let imageProvider = CLKImageProvider(onePieceImage: UIImage(named: "Complication/Modular")!)
+            let textProvider = CLKTextProvider(format: "待ち時間")
+            template = CLKComplicationTemplateModularSmallStackImage(line1ImageProvider: imageProvider,
+                                                                     line2TextProvider: textProvider)
+        case .utilitarianSmall:
+            let imageProvider = CLKImageProvider(onePieceImage: UIImage(named: "Complication/Utilitarian")!)
+            template = CLKComplicationTemplateUtilitarianSmallSquare(imageProvider: imageProvider)
+        case .utilitarianSmallFlat:
+            let imageProvider = CLKImageProvider(onePieceImage: UIImage(named: "Complication/Utilitarian")!)
+            let textProvider = CLKTextProvider(format: "待ち時間")
+            template = CLKComplicationTemplateUtilitarianSmallFlat(textProvider: textProvider,
+                                                                   imageProvider: imageProvider)
+        case .graphicRectangular, .modularLarge, .utilitarianLarge, .extraLarge, .graphicExtraLarge, .graphicBezel:
+            handler(nil)
+            return
+        @unknown default:
+            handler(nil)
+            return
+        }
+
+        template.tintColor = .tdlTheme
+
+        handler(CLKComplicationTimelineEntry(date: Date(), complicationTemplate: template))
     }
 
     // MARK: - Sample Templates
     
     func getLocalizableSampleTemplate(for complication: CLKComplication, withHandler handler: @escaping (CLKComplicationTemplate?) -> Void) {
-        // This method will be called once per supported complication, and the results will be cached
-        handler(nil)
+        let template: CLKComplicationTemplate
+
+        switch complication.family {
+        case .circularSmall:
+            let imageProvider = CLKImageProvider(onePieceImage: UIImage(named: "Complication/Circular")!)
+            template = CLKComplicationTemplateCircularSmallSimpleImage(imageProvider: imageProvider)
+        case .graphicCircular:
+            template = CLKComplicationTemplateGraphicCircularView(Image("Complication/Graphic Circular"))
+        case .graphicCorner:
+            let imageProvider = CLKFullColorImageProvider(fullColorImage: UIImage(named: "Complication/Graphic Corner")!)
+            template = CLKComplicationTemplateGraphicCornerCircularImage(imageProvider: imageProvider)
+        case .modularSmall:
+            let imageProvider = CLKImageProvider(onePieceImage: UIImage(named: "Complication/Modular")!)
+            let textProvider = CLKTextProvider(format: "待ち時間")
+            template = CLKComplicationTemplateModularSmallStackImage(line1ImageProvider: imageProvider,
+                                                                     line2TextProvider: textProvider)
+        case .utilitarianSmall:
+            let imageProvider = CLKImageProvider(onePieceImage: UIImage(named: "Complication/Utilitarian")!)
+            template = CLKComplicationTemplateUtilitarianSmallSquare(imageProvider: imageProvider)
+        case .utilitarianSmallFlat:
+            let imageProvider = CLKImageProvider(onePieceImage: UIImage(named: "Complication/Utilitarian")!)
+            let textProvider = CLKTextProvider(format: "待ち時間")
+            template = CLKComplicationTemplateUtilitarianSmallFlat(textProvider: textProvider,
+                                                                   imageProvider: imageProvider)
+        case .graphicRectangular, .modularLarge, .utilitarianLarge, .extraLarge, .graphicExtraLarge, .graphicBezel:
+            handler(nil)
+            return
+        @unknown default:
+            handler(nil)
+            return
+        }
+
+        template.tintColor = .tdlTheme
+
+        handler(template)
     }
 }
