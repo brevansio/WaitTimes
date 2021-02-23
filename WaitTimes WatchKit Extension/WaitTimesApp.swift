9f9ec0a46b375f2eaaf7e13a1325b13695609254
Bruce Evans <bruce.evans.dev@gmail.com>
2021-02-23 13:12:40 +0900

    Add refresh on becoming active

--- WaitTimes WatchKit Extension/WaitTimesApp.swift
+++ WaitTimes WatchKit Extension/WaitTimesApp.swift
@@ -9,19 +9,31 @@ import SwiftUI
 
 @main
 struct WaitTimesApp: App {
+    @Environment(\.scenePhase) private var scenePhase
+
     var landImageDataModel = ImageDataModel()
     var seaImageDataModel = ImageDataModel()
+
+    var landFacilityDataModel = FacilityDataModel(location: .land)
+    var seaFacilityDataModel = FacilityDataModel(location: .sea)
+
 
     var body: some Scene {
         WindowGroup {
             TabView {
-                WaitListView(viewModel: FacilityDataModel(location: .land)).environmentObject(landImageDataModel)
-                WaitListView(viewModel: FacilityDataModel(location: .sea)).environmentObject(seaImageDataModel)
+                WaitListView(viewModel: landFacilityDataModel).environmentObject(landImageDataModel)
+                WaitListView(viewModel: seaFacilityDataModel).environmentObject(seaImageDataModel)
             }
             .tabViewStyle(PageTabViewStyle())
             .onAppear(perform: {
                 landImageDataModel.loadImages(for: .land)
                 seaImageDataModel.loadImages(for: .sea)
+            })
+            .onChange(of: scenePhase, perform: { newPhase in
+                if newPhase == .active {
+                    landFacilityDataModel.load()
+                    seaFacilityDataModel.load()
+                }
             })
         }
     }
